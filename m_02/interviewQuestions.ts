78693ea2b71ae467de8ff017807ec1b2c2bd2d3a
moriHe <mor.hettich@gmail.com>
2026-07-04 15:40:20 +0200

    Some improvements to modularization of UnionFind

--- m_02/interviewQuestions.ts
+++ m_02/interviewQuestions.ts
@@ -17,45 +17,79 @@ interface LogEntry {
     iB: number;
 }
 
+interface UnionDataType {
+    union(p: number, q: number): void
+    connected(p: number, q: number): boolean;
+    find(i: number): number;
+}
+
+class UnionFind implements UnionDataType  {
+    private parents: number[];
+    private sizes: number[];
+
+    constructor(n: number) {
+        this.parents = Array.from({ length: n }, (_, i) => i);
+        this.sizes = Array(n).fill(1);
+    }
+
+    public find(i: number): number {
+        let rootIndex = i;
+        while (rootIndex !== this.parents[rootIndex]) {
+            rootIndex = this.parents[rootIndex];
+        } 
+
+        return rootIndex;
+    }
+
+    public connected(p: number, q: number): boolean {
+        return this.find(p) === this.find(q);
+    }
+
+    public union(p: number, q: number) {
+        let rootAIndex = this.find(p);
+        let rootBIndex = this.find(q);
+        let sizeA = this.sizes[rootAIndex];
+        let sizeB = this.sizes[rootBIndex];
+        let sizeSum = sizeA + sizeB;
+        if (sizeA >= sizeB) {
+            this.sizes[rootAIndex] = sizeSum;
+            this.parents[rootBIndex] = rootAIndex;
+        } else {
+            this.sizes[rootBIndex] = sizeSum;
+            this.parents[rootAIndex] = rootBIndex;            
+        }
+    }
+}
+
+
+
+
 function earliestTimestampAllConnected(n: number, logs: LogEntry[]): number | null {
     let group = n;
-    const parents = Array.from({ length: n }, (_, i) => i);
-    const sizes = Array(n).fill(1);
-
+    let UnionFindInstance = new UnionFind(n);
     for (let i = 0; i < logs.length; i++) {
 
         const currAIndex = logs[i].iA;
         const currBIndex = logs[i].iB;
 
-        let rootAIndex = currAIndex;
-        while (rootAIndex !== parents[rootAIndex]) {
-            rootAIndex = parents[rootAIndex];
-        }
-
-        let rootBIndex = currBIndex;
-        while (rootBIndex !== parents[rootBIndex]) {
-            rootBIndex = parents[rootBIndex];
-        }
-
-        if (rootBIndex === rootAIndex) {
+        if (UnionFindInstance.connected(currAIndex, currBIndex)) 
             continue;
-        }
-
-        let sizeA = sizes[rootAIndex];
-        let sizeB = sizes[rootBIndex];
-        let sizeSum = sizeA + sizeB;
-        if (sizeA >= sizeB) {
-            sizes[rootAIndex] = sizeSum;
-            parents[rootBIndex] = rootAIndex;
-        } else {
-            sizes[rootBIndex] = sizeSum;
-            parents[rootAIndex] = rootBIndex;            
-        }
-
+        
+        UnionFindInstance.union(currAIndex, currBIndex);
         group--;
         if (group === 1) {
             return logs[i].timestamp;
         }
+
     }
     return null;
 }
+
+/*
+Union-find mit spezifischem kanonischen Element. Fügen Sie eine Methode find() zum union-find Datentyp hinzu,
+so dass find(i) das größte Element in der verbundenen Komponente zurückgibt, die i enthält. Die Operationen union(),
+connected() und find() sollten alle logarithmische Zeit oder mehr benötigen.
+
+Wenn zum Beispiel eine der verbundenen Komponenten {1, 2, 6, 9} ist, dann sollte die Methode find() für jedes der vier
+Elemente in den verbundenen Komponenten 9 zurückgeben.
+*/
