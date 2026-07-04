e736f29de112ca616496a574534116663ac99fdc
moriHe <mor.hettich@gmail.com>
2026-07-04 14:55:08 +0200

    Finish m02 interview question 1

--- m_02/interviewQuestions.ts
+++ m_02/interviewQuestions.ts
@@ -11,15 +11,53 @@ Freundschaft eine Äquivalenzbeziehung ist. Die Laufzeit Ihres Algorithmus sollt
 log n oder besser sein und zusätzlichen Speicherplatz proportional zu n verbrauchen.
 */
 
-function earliestTimestampAllConnected(n: number, m: number): number | null {
+interface LogEntry {
+    timestamp: number;
+    iA: number;
+    iB: number;
+}
+
+function earliestTimestampAllConnected(n: number, logs: LogEntry[]): number | null {
     let group = n;
-    let parents = [Array(n).keys()];
+    const parents = Array.from({ length: n }, (_, i) => i);
+    const sizes = Array(n).fill(1);
 
-    for (let i = 0; i < m; i++) {
+    for (let i = 0; i < logs.length; i++) {
 
+        const currAIndex = logs[i].iA;
+        const currBIndex = logs[i].iB;
+
+        let rootAIndex = currAIndex;
+        while (rootAIndex !== parents[rootAIndex]) {
+            rootAIndex = parents[rootAIndex];
+        }
+
+        let rootBIndex = currBIndex;
+        while (rootBIndex !== parents[rootBIndex]) {
+            rootBIndex = parents[rootBIndex];
+        }
+
+        if (rootBIndex === rootAIndex) {
+            continue;
+        }
+
+        let sizeA = sizes[rootAIndex];
+        let sizeB = sizes[rootBIndex];
+        let sizeSum = sizeA + sizeB;
+        if (sizeA >= sizeB) {
+            sizes[rootAIndex] = sizeSum;
+            parents[rootBIndex] = rootAIndex;
+        } else {
+            sizes[rootBIndex] = sizeSum;
+            parents[rootAIndex] = rootBIndex;            
+        }
+
+        group--;
+        if (group === 1) {
+            return logs[i].timestamp;
+        }
     }
     return null;
-
 }
 
 /*
