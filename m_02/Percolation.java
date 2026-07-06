0daabe81f0c74dbbf055b5514adb1d2ebc7a0844
moriHe <mor.hettich@gmail.com>
2026-07-06 21:20:05 +0200

    Setup Perlocation Exercise

---
+++ m_02/Percolation.java
@@ -0,0 +1,45 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        // TODO: Implement
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        // TODO: Implement
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        // TODO: Implement
+        return true;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        // TODO: Implement
+        return true;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        // TODO: Implement
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        // TODO: Implement
+        return true;
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+        // TODO: Implement
+    }
+}
---
+++ m_02/PercolationStats.java
@@ -0,0 +1,39 @@
+public class PercolationStats {
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        // TODO: Implement
+        return;
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        // TODO: Implement
+        return 1.0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        // TODO: Implement
+        return 1.0;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        // TODO: Implement
+        return 1.0;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        // TODO: Implement
+        return 1.0;
+    }
+
+   // test client (see below)
+   public static void main(String[] args) {
+    // TODO: Implement
+    return;
+   }
+
+}
