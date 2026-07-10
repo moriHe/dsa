9a4c9144548eca2cb9e46959f64169fc606b6856
moriHe <mor.hettich@gmail.com>
2026-07-10 23:18:38 +0200

    Finish Percolation Exercise

--- m_02/Percolation.java
+++ m_02/Percolation.java
@@ -1,14 +1,13 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private boolean[] openSites;
     private final int n;
-    WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF uFull;
     private final int virtualTop;
     private final int virtualBottom;
-
+    private int openCount;
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
@@ -17,8 +16,10 @@ public class Percolation {
         this.n = n;
         this.openSites = new boolean[n*n];
         this.uf = new WeightedQuickUnionUF(n * n + 2);
+        this.uFull = new WeightedQuickUnionUF(n * n + 1);
         this.virtualTop = n * n;
         this.virtualBottom = n * n + 1;
+        this.openCount = 0;
     }
 
     private int getIndex(int row, int col) {
@@ -33,22 +34,49 @@ public class Percolation {
         return false;
     }
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col) throws IllegalArgumentException {
+    public void open(int row, int col) {
         if (isOutOfBounce(col) || isOutOfBounce(row)) {
             throw new IllegalArgumentException("col or row out of bounce");
         }
 
+        if (isOpen(row, col)) {
+            return;
+        }
         int i = getIndex(row, col);
         this.openSites[i] = true;
+        this.openCount++;
         if (row == 1) {
             this.uf.union(i, this.virtualTop);
+            this.uFull.union(i, this.virtualTop);
         } 
         
         if (row == n) {
             this.uf.union(i, this.virtualBottom);
+        } 
+
+        if (row < n && isOpen(row + 1, col)) {
+            int bottom = getIndex(row + 1, col);
+            uf.union(i, bottom);
+            uFull.union(i, bottom);
         }
 
-        // TODO: Check left right top bottom are isOpen and union in that case
+        if (row > 1 &&  isOpen(row - 1, col)) {
+            int top = getIndex(row - 1, col);
+            uf.union(i, top);
+            uFull.union(i, top);
+        }
+
+        if (col < n &&  isOpen(row, col + 1)) {
+            int right = getIndex(row, col + 1);
+            uf.union(i, right);
+            uFull.union(i, right);
+        }
+
+        if (col > 1 && isOpen(row, col - 1)) {
+            int left = getIndex(row, col - 1);
+            uf.union(i, left);
+            uFull.union(i, left);
+        }
     }
 
     // is the site (row, col) open?
@@ -56,8 +84,8 @@ public class Percolation {
         if (isOutOfBounce(col) || isOutOfBounce(row)) {
             throw new IllegalArgumentException("col or row out of bounce");
         }
-        // TODO: Implement
-        return true;
+        
+        return openSites[getIndex(row, col)];
     }
 
     // is the site (row, col) full?
@@ -65,24 +93,35 @@ public class Percolation {
         if (isOutOfBounce(col) || isOutOfBounce(row)) {
             throw new IllegalArgumentException("col or row out of bounce");
         }
-        // TODO: Implement
-        return true;
+        
+        return isOpen(row, col) && this.uFull.find(getIndex(row, col)) == uFull.find(virtualTop);
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        // TODO: Implement
-        return 0;
+        return openCount;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        // TODO: Implement
-        return true;
+        return uf.find(this.virtualBottom) == uf.find(this.virtualTop);
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        // TODO: Implement
+        int n = 3;
+        Percolation perc = new Percolation(n);
+
+        perc.open(1,1);
+        perc.open(2,1);
+        perc.open(3,1);
+
+        if (perc.percolates()) {
+            System.out.println("System percolates. Success!");
+        } else {
+            System.out.println("System does not percolate. Fail!");
+        }
+
+        System.out.println("(1,2) == full? " + perc.isFull(1,2));
     }
 }
--- m_02/PercolationStats.java
+++ m_02/PercolationStats.java
@@ -1,39 +1,62 @@
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    private double[] results;
+    private final int rounds;
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
-        // TODO: Implement
-        return;
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("n or trials is 0 or smaller.");
+        }
+
+        this.results = new double[trials];
+        this.rounds = trials;
+
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            int openCount = 0;
+            while (!p.percolates()) {
+                int row = StdRandom.uniformInt(1, n + 1);
+                int col = StdRandom.uniformInt(1, n + 1);
+                if (!p.isOpen(row, col)) {
+                    p.open(row, col);
+                    openCount++;
+                }
+            }
+
+            this.results[i] = (double) openCount / (n * n);
+        }
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        // TODO: Implement
-        return 1.0;
+        return StdStats.mean(results);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        // TODO: Implement
-        return 1.0;
+        return StdStats.stddev(results);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        // TODO: Implement
-        return 1.0;
+        return mean() - (1.96 / Math.sqrt(rounds));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        // TODO: Implement
-        return 1.0;
+        return mean() + (1.96 / Math.sqrt(rounds));
     }
 
    // test client (see below)
    public static void main(String[] args) {
-    // TODO: Implement
-    return;
+    int n = Integer.parseInt(args[0]);
+    int trials = Integer.parseInt(args[1]);
+    PercolationStats ps = new PercolationStats(n, trials);
+    System.out.println("mean                    = " + ps.mean());
+    System.out.println("stddev                  = " + ps.stddev());
+    System.out.println("95% confidence interval = [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
    }
 
 }
