75012150ad603462103bd6f809db87911d713263
moriHe <mor.hettich@gmail.com>
2026-07-26 12:07:44 +0200

    Continuing exercise RandomizedQueue

--- m_04/RandomizedQueue.java
+++ m_04/RandomizedQueue.java
@@ -1,34 +1,62 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-
+    private Item[] array;
+    private int minCapacity;
+    private int size;
     // construct an empty randomized queue
     public RandomizedQueue() {
-
+        this.array = (Item[]) new Object[1];
+        this.size = 0;
+        this.minCapacity = 1;
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return true;
+        return this.size == 0;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        return 1;
+        return this.size;
     }
 
     // add the item
     public void enqueue(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
 
+        if (this.size == this.array.length) {
+            Item[] newArray = (Item[]) new Object[this.array.length * 2];
+            for (int i = 0; i < this.array.length; i++) {
+                newArray[i] = this.array[i];
+            }
+            this.array = newArray;
+        }
+
+        this.array[size] = item;
+        size++;
     }
 
     // remove and return a random item
     public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        // Remove at random
+        // Take last element of array and put it in the empty spot, decrease size
+        // array.length > minCapacity && size <= array.length / 4 -> half capacity of array
+
         return null;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
         return null;
     }
 
@@ -41,4 +69,36 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public static void main(String[] args) {
     }
 
+    private class Node {
+        Item item;
+        Node next;
+        Node prev;
+        Node(Item item) {
+            this.item = item;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    private class IteratorImpl implements Iterator<Item> {
+        Item current;
+        IteratorImpl(Item item) {
+            this.current = item;
+        }
+
+        public boolean hasNext() {
+            return this.current != null;
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("No more nodes in list");
+            }
+            return null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("Method remove is not supported.");
+        }
+    }
 }
