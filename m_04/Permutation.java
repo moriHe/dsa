964c5c02d9453ea92ac5c4edde4b1129a20ea043
moriHe <mor.hettich@gmail.com>
2026-07-27 09:04:14 +0200

    Finish exercise m_04

--- m_04/Permutation.java
+++ m_04/Permutation.java
@@ -1,5 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
    public static void main(String[] args) {
-    
+      int k = Integer.parseInt(args[0]);
+
+      RandomizedQueue<String> rd = new RandomizedQueue<>();
+      while (!StdIn.isEmpty()) {
+         String s = StdIn.readString();
+         rd.enqueue(s);
+      }
+
+      for (int i = 0; i < k; i++) {
+         System.out.println(rd.dequeue());
+      }
    }
 }
