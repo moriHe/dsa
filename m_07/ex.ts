b3a361426a77061327d1fc0ae9fc6f98be9b3e03
moriHe <mor.hettich@gmail.com>
2026-08-28 22:09:01 +0200

    finish ex 1/3

--- m_07/ex.ts
+++ m_07/ex.ts
@@ -34,95 +34,35 @@ function exch(arr: number[], a: number, b: number) {
     arr[b] = tmp;
 }
 
-function placePivot(arr: number[], pivot: number, lo: number, hi: number) {
-        let i = lo;
-        let j = hi;
-        let isHigh = false;
-                    console.log("***")
-        let matchIdx: number | null = null;
-        // TODO: Problem right now is that I remember the match idx, although the place I find the match to the pivot
-        // has a high chance of not being the correct partitioning position.
-        // The challenge now is to find the matchidx and move it left/right if there is a higher/smaller number coming and remember that
-        // shift
-        while (true) {
-            while (less(arr[i], pivot)) {
-                if (i === hi) {
-                    if (arr[i] === pivot) matchIdx = i;
-                    break;
-                }
-                i++;
-            }
-            if (arr[i] > pivot && matchIdx !== null) {
-                let tmp = arr[i];
-                arr[i] = arr[matchIdx]
-                arr[matchIdx] = tmp;
-                matchIdx = i;
-            }
-            while (less(pivot, arr[j])) {
-                if (j === lo) {
-                    if (arr[j] === pivot) matchIdx = j;
-                    break;
-                }
-                j--;
-            }
-            if (i <= j) break;
+function placePivot(arr: number[], pivot: number, lo: number, hi: number): number {
+       for (let i = lo; i <= hi; i++) {
             if (arr[i] === pivot) {
-                matchIdx = i;
-                console.log("i="+i)
-                i++;
-            } else if (arr[j] === pivot) {
-                matchIdx = j;
-                console.log("j="+j)
-                j--;
-            } else {
-                exch(arr, i, j);
+                let tmp = arr[lo];
+                arr[lo] = arr[i];
+                arr[i] = tmp;
+                break;
             }
-        }
-        console.log("matchidx="+matchIdx)
-                    console.log("***")
-    
-    return matchIdx;
-}
-/*
-function placePivot(arr: number[], pivot: number, lo: number, hi: number) {
-        let i = lo;
+       }
+
+        let i = lo+1;
         let j = hi;
-        let isHigh = false;
-        while (true) {
+       while (true) {
             while (less(arr[i], pivot)) {
                 if (i === hi) break;
                 i++;
             }
-
             while (less(pivot, arr[j])) {
-                if (j == lo) break;
-                j--;
-            }
-            if (i >= j) break;
-
-            if (arr[i] === pivot) {
-                let tmp = arr[i];
-                arr[i] = arr[lo];
-                arr[lo] = tmp;
-                if (i === lo) i++;
-                if (i === hi) break;
-            } else if (arr[j] === pivot) {
-                let tmp = arr[j];
-                arr[j] = arr[hi];
-                arr[hi] = tmp;
-                isHigh = true;
-                if (j === hi) j--;
                 if (j === lo) break;
-            } else {
-                exch(arr, i, j);
+                j--;
             }
 
-    }
-    console.log("ishigh="+isHigh+", hi="+hi+", lo="+lo+", j="+j)
-    exch(arr, isHigh ? hi : lo, j)
-    return j;
+            if (i >= j) break;
+            exch(arr, i, j)
+       }
+       exch(arr, lo, j);
+       return j;
 }
-    */
+
 
 function quickSort(arr: number[], lo: number, hi: number, pivotIdx: number, pivotArr: number[]) {
     if ((hi - lo) <= 0) {
@@ -132,9 +72,6 @@ function quickSort(arr: number[], lo: number, hi: number, pivotIdx: number, pivo
 
     let matchIdx = placePivot(arr, pivotArr[pivotIdx], lo, hi)
     if (matchIdx !== null) {
-        let tmp = pivotArr[pivotIdx];
-        pivotArr[pivotIdx] = pivotArr[matchIdx]
-        pivotArr[matchIdx] = tmp;
         placePivot(pivotArr, arr[matchIdx], lo, hi)
         if (matchIdx > lo)        
             quickSort(arr, lo, matchIdx - 1, lo, pivotArr)
