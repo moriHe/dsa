6192c1988dd467d0805a915336ad01e869bc950c
moriHe <mor.hettich@gmail.com>
2026-09-12 20:40:29 +0200

    finish not teted m08/ex1

--- m_08/ex.ts
+++ m_08/ex.ts
@@ -87,6 +87,60 @@ class DynamicMedian {
         }
     }
 
+    bubbleDownMaxHeap() {
+        let parentIndex = 0
+        let childIndexLeft = parentIndex * 2 + 1
+        let childIndexRight = parentIndex * 2 + 2
+        while (true) {
+            if (childIndexLeft >= this.maxHeap.length) {
+                return
+            }
+
+            if (childIndexRight >= this.maxHeap.length) {
+                if (this.maxHeap[parentIndex] >= this.maxHeap[childIndexLeft]) {
+                    return
+                }
+                let tmp = this.maxHeap[parentIndex]
+                this.maxHeap[parentIndex] = this.maxHeap[childIndexLeft]
+                this.maxHeap[childIndexLeft] = tmp
+                return
+            }
+            if (this.maxHeap[parentIndex] >= this.maxHeap[childIndexLeft] && this.maxHeap[parentIndex] >= this.maxHeap[childIndexRight]) {
+                return
+            }
+            if (this.maxHeap[childIndexLeft] >= this.maxHeap[childIndexRight]) {
+                let tmp = this.maxHeap[parentIndex]
+                this.maxHeap[parentIndex] = this.maxHeap[childIndexLeft]
+                this.maxHeap[childIndexLeft] = tmp
+                parentIndex = childIndexLeft
+            } else {
+                let tmp = this.maxHeap[parentIndex]
+                this.maxHeap[parentIndex] = this.maxHeap[childIndexRight]
+                this.maxHeap[childIndexRight] = tmp
+                parentIndex = childIndexRight
+            }
+            childIndexLeft = parentIndex * 2 + 1
+            childIndexRight = parentIndex * 2 + 2
+
+        }
+    }
+
+    rebalance() {
+        if (this.minHeap.length > this.maxHeap.length) {
+            this.maxHeap[this.maxHeap.length] = this.minHeap[0]
+            this.bubbleUpMaxHeap()
+            this.minHeap[0] = this.minHeap[this.minHeap.length - 1]
+            this.minHeap.pop()
+            this.bubbleDownMinHeap()
+        } else if (this.maxHeap.length > this.minHeap.length + 1) {
+            this.minHeap[this.minHeap.length] = this.maxHeap[0]
+            this.bubbleUpMinHeap()
+            this.maxHeap[0] = this.maxHeap[this.maxHeap.length - 1]
+            this.maxHeap.pop()
+            this.bubbleDownMaxHeap()
+        }
+    }
+
     insert(x: number) {
         if (this.minHeap.length === 0 && this.maxHeap.length === 0) {
             this.maxHeap[0] = x;
@@ -102,30 +156,20 @@ class DynamicMedian {
             this.bubbleUpMaxHeap()
         }
 
-        if (this.minHeap.length > this.maxHeap.length) {
-            this.maxHeap[this.maxHeap.length] = this.minHeap[0]
-            this.minHeap[0] = this.minHeap[this.minHeap.length - 1]
-            this.minHeap.pop()
-            this.bubbleUpMaxHeap()
-        }
-        /**
-
-         * 4) if minHeap is larger than maxHeap, put minHeap[0] at end of maxHeap bubble up
-         * 5) if maxHeap is > minHeap + 1, put maxHeap[0] at end of minHeap bubble up
-         */
+        this.rebalance()
     }
 
     median(): number {
         return this.maxHeap[0]
     }
 
-    removeMedian() {
-        /**
-         * 1) swap maxHeap[0] with last element
-         * 2) remove last element, store it for return
-         * 3) sink down maxHeap[0]
-         * 4) rebalance if needed
-         */
+    removeMedian(): number {
+        let median = this.maxHeap[0]
+        this.maxHeap[0] = this.maxHeap[this.maxHeap.length - 1]
+        this.maxHeap.pop()
+        this.bubbleDownMaxHeap()
+        this.rebalance()
+        return median
     }
 
 }
@@ -133,28 +177,26 @@ class DynamicMedian {
 const dm = new DynamicMedian()
 
 dm.insert(5)
-console.log("median:", dm.median()) // 5
+console.log("expected: 5, median:", dm.median()) // 5
 
 dm.insert(10)
-console.log("median:", dm.median()) // 5
+console.log("expected: 5, median:", dm.median()) // 5
 
 dm.insert(3)
-console.log("median:", dm.median()) // 5
+console.log("expected: 5, median:", dm.median()) // 5
 
 dm.insert(8)
-console.log("median:", dm.median()) // 5
+console.log("expected: 5, median:", dm.median()) // 5
 
 dm.insert(1)
-console.log("median:", dm.median()) // 5
+console.log("expected: 5, median:", dm.median()) // 5
 
 dm.insert(7)
-console.log("median:", dm.median()) // 5
+console.log("expected: 5, median:", dm.median()) // 5
 
-console.log("maxHeap:", dm.maxHeap)
-console.log("minHeap:", dm.minHeap)
 
-console.log("removed:", dm.removeMedian()) // 5
-console.log("new median:", dm.median())    // 7
+console.log("expected: 5, removed:", dm.removeMedian()) // 5
+console.log("expected: 7, new median:", dm.median())    // 7
 
-console.log("removed:", dm.removeMedian()) // 7
-console.log("new median:", dm.median())    // 3
+console.log("expected: 7, removed:", dm.removeMedian()) // 7
+console.log("expected: 3, new median:", dm.median())    // 3
